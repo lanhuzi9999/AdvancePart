81568b0b778d1e621df722fd2c6a9f6f84f3e892
lanhuzi9999 <984566200@qq.com>
2014-09-18 00:58:33 +0800

    Revert "day02-day11"
    This reverts commit 82db4cf3799ec1422f3aea0533664ea5bee0b951.

--- day10/project1308/androidProject/gen/com/example/androidproject/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.androidproject;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- day10/project1308/androidProject/src/com/example/androidproject/Math.java
+++
@@ -1,9 +0,0 @@
-package com.example.androidproject;
-
-public class Math {
-
-
-	public int add(int n1, int n2) {
-		return n1 + n2;
-	}
-}
